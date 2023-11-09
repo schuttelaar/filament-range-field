546756591c2cf914eea2724396a109cb5778f8a1
Bram Ueffing <bram.ueffing@schuttelaar.nl>
2023-11-09 16:55:35 +0100

    support tooltips

--- resources/views/forms/components/_range-slider-step.blade.php
+++ resources/views/forms/components/_range-slider-step.blade.php
@@ -1,10 +1,11 @@
 <li @click="state = {{ $state }}">
     <span
-        class="cursor-pointer text-xs hover:text-primary-500 dark:hover:text-primary-500 font-medium leading-4 text-gray-700 dark:text-gray-300"
+        class="cursor-pointer text-sm hover:text-primary-500 dark:hover:text-primary-500 font-medium leading-4 text-gray-700 dark:text-gray-300"
         x-bind:class="{
             'dark:text-primary-500 text-primary-500': state === {{ $state }},
         }"
+         @isset($step['t']) x-tooltip.raw="{{$step['t']}}" @endisset
     >
-        {{ $step }}
+        {{ $step['label'] }}
     </span>
 </li>
